74c84690018c3fd3b2d6a64fc80287b9f5489961
Marc Zimmermann <marc.zimmermann@mmbash.de>
2017-01-30 14:10:50 +0100

    chore(codeship): Test codeship workflow.

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#set -e
+
+SLOPPY_APITOKEN=${SLOPPY_APITOKEN}
+
+# install latest sloppy.io cli
+curl -L https://files.sloppy.io/sloppy-`uname -s`-`uname -m` > /usr/local/bin/sloppy
+chmod +x /usr/local/bin/sloppy
+
+# deploy new image
+echo "Deploying new image using the CLI"
+sloppy change -img mikemichel/hackathon-starter:$CI_COMMIT_ID
